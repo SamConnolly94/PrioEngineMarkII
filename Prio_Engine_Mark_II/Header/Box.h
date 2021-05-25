cec28b3bb05876782202f5bd19f828509caf519a
Sam Connolly <samconnolly94@gmail.com>
2021-05-25 16:42:14 +0100

    Fixed issue with boxes not rendering correctly.

--- Prio_Engine_Mark_II/Header/Box.h
+++ Prio_Engine_Mark_II/Header/Box.h
@@ -19,44 +19,8 @@ namespace PrioEngineII
 	public:
 		Box(ComPtr<DirectXDeviceType> d3dDevice, ComPtr<CommandListType> commandList, std::string& name)
 		{
-			vertices =
-			{
-				Vertex({ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT4(Colors::White) }),
-				Vertex({ XMFLOAT3(-1.0f, +1.0f, -1.0f), XMFLOAT4(Colors::Black) }),
-				Vertex({ XMFLOAT3(+1.0f, +1.0f, -1.0f), XMFLOAT4(Colors::Red) }),
-				Vertex({ XMFLOAT3(-1.0f, -1.0f, +1.0f), XMFLOAT4(Colors::Blue) }),
-				Vertex({ XMFLOAT3(+1.0f, -1.0f, -1.0f), XMFLOAT4(Colors::Green) }),
-				Vertex({ XMFLOAT3(-1.0f, +1.0f, +1.0f), XMFLOAT4(Colors::Yellow) }),
-				Vertex({ XMFLOAT3(+1.0f, +1.0f, +1.0f), XMFLOAT4(Colors::Cyan) }),
-				Vertex({ XMFLOAT3(+1.0f, -1.0f, +1.0f), XMFLOAT4(Colors::Magenta) })
-			};
-
-			indices =
-			{
-				// front face
-				0, 1, 2,
-				0, 2, 3,
-
-				// back face
-				4, 6, 5,
-				4, 7, 6,
-
-				// left face
-				4, 5, 1,
-				4, 1, 0,
-
-				// right face
-				3, 2, 6,
-				3, 6, 7,
-
-				// top face
-				1, 5, 6,
-				1, 6, 2,
-
-				// bottom face
-				4, 0, 3,
-				4, 3, 7
-			};
+			InitialiseVertices();
+			InitialiseIndices();
 
 			mBoxGeometry = std::make_unique<MeshGeometry>();
 			mName = name;
@@ -88,12 +52,16 @@ namespace PrioEngineII
 			mBoxGeometry->DrawArgs["box"] = submesh;
 		}
 
-	public:
+
+		~Box()
+		{
+		}
+
 		constexpr UINT GetVertexBufferByteSize() const
 		{
 			return (UINT)vertices.size() * sizeof(Vertex);
 		}
-		
+
 		constexpr UINT GetIndexCount() const
 		{
 			return (UINT)indices.size();
@@ -112,6 +80,52 @@ namespace PrioEngineII
 		D3D12_INDEX_BUFFER_VIEW GetIndexBufferView() const
 		{
 			return mBoxGeometry->IndexBufferView();
+		}
+	private:
+
+		void InitialiseIndices()
+		{
+			indices =
+			{
+				// front face
+				0, 1, 2,
+				0, 2, 3,
+
+				// back face
+				4, 6, 5,
+				4, 7, 6,
+
+				// left face
+				4, 5, 1,
+				4, 1, 0,
+
+				// right face
+				3, 2, 6,
+				3, 6, 7,
+
+				// top face
+				1, 5, 6,
+				1, 6, 2,
+
+				// bottom face
+				4, 0, 3,
+				4, 3, 7
+			};
+		}
+
+		void InitialiseVertices()
+		{
+			vertices =
+			{
+				Vertex({ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT4(Colors::White) }),
+				Vertex({ XMFLOAT3(-1.0f, +1.0f, -1.0f), XMFLOAT4(Colors::Black) }),
+				Vertex({ XMFLOAT3(+1.0f, +1.0f, -1.0f), XMFLOAT4(Colors::Red) }),
+				Vertex({ XMFLOAT3(+1.0f, -1.0f, -1.0f), XMFLOAT4(Colors::Green) }),
+				Vertex({ XMFLOAT3(-1.0f, -1.0f, +1.0f), XMFLOAT4(Colors::Blue) }),
+				Vertex({ XMFLOAT3(-1.0f, +1.0f, +1.0f), XMFLOAT4(Colors::Yellow) }),
+				Vertex({ XMFLOAT3(+1.0f, +1.0f, +1.0f), XMFLOAT4(Colors::Cyan) }),
+				Vertex({ XMFLOAT3(+1.0f, -1.0f, +1.0f), XMFLOAT4(Colors::Magenta) })
+			};
 		}
 	};
 }
